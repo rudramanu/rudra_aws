317e2f615d00e753c49fa462153dbc8d9234202b
Rudra <merudramanu@gmail.com>
2023-03-19 00:24:20 +0530

    added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require("express")();
 
 app.get("/home", (req, res) => {
-  res.send("Getting server 1");
+  res.send("Getting server so be happy");
 });
 
 app.listen(5600, () => {
   console.log("Connected to server");
 });
+//====================
